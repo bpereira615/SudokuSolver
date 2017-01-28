a9dab0264699ebf2d03c5ab1f2b33fa92e891bab
Benjamin Hoertnagl-Pereira <bhoertn1@jhu.edu>
2017-01-28 13:09:22 -0500

    Initial commit, began cross() and grid_values()

--- solution.py
+++ solution.py
@@ -24,12 +24,46 @@ def naked_twins(values):
     # Eliminate the naked twins as possibilities for their peers
 
 def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+    """Cross product of elements in A and elements in B.
+        
+    Args:
+        A, B: two strings
+    Returns:
+        the list formed by all the possible concatenations of a 
+        letter s in string A with a letter t in string B
+    """
+    
+    return [s+t for s in a for t in b]
+
 
 def grid_values(grid):
-    "Convert grid into a dict of {square: char} with '.' for empties."
-    pass
+    """Convert grid into a dict of {square: char} with '.' for empties.
+    
+    
+    Args:
+        grid: Sudoku grid in string form, 81 characters long
+    Returns:
+        Sudoku grid in dictionary form:
+        - keys: Box labels, e.g. 'A1'
+        - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
+    """
+
+    dict = {}
+    
+    index = 0
+    for j in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']:
+        for i in range(1, 10):
+        
+            curr = j+str(i)
+
+            if grid[index] == '.':
+                dict[curr] = '123456789'
+            else:
+                dict[curr] = grid[index]
+            
+            index += 1
+            
+    return dict
 
 def display(values):
     "Display these values as a 2-D grid."
